6062297836ceb42935f96fa3b9cba2f6328e2cee
Juan Pablo <juan.reale@claro.com.ar>
2020-04-20 23:24:27 -0300

    Plantilla - Se crea la base de la plantilla general

---
+++ Integrador/resources/views/plantilla/plantillaGeneral.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+
+    <!-- VIEWPORT - Para que nuestro proyecto sea adaptable de PC a Smarphone. -->
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/stilos.css">
+
+	<!-- ICONOS -->
+	<link rel="stylesheet" href="css/fontsMenu.css">
+    
+    <title>PROYECTO FINAL</title>
+</head>
+<body>
+    <header>
+        <h1>BIENVENIDOS</h1>
+    </header>
+    
+    <section>
+        @yield("Principal")
+    </section>
+
+    <footer>
+        <h3>PROYECTO ELEVORADO POR JUAN REALE</h3>    
+    </footer>
+
+</body>
+</html>
